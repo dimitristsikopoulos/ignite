9a4a2b49f92bd1e69e0cb6e5bdc2628d2189171f
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-11-28 11:14:41 +0200

    7. Refactoring To Home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import React from "react";
+//Components and Pages
+import Home from "./pages/Home";
 
 function App() {
-  const dispach = useDispatch();
-  useEffect(() => {
-    dispach(loadGames());
-  });
-
   return (
     <div className="App">
       <h1>Hello Ignite</h1>
+      <Home />
     </div>
   );
 }
