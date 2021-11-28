b903941d2cdc1f27d96cc47944a4d374d1f53f79
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-11-28 16:17:54 +0200

    10. Fetch Game Detail

--- src/api.js
+++ src/api.js
@@ -36,3 +36,9 @@ const newGames = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
 export const newGamesURL = () => `${base_url}${newGames}`;
+//GAME DETAILS
+export const gameDetailsURL = (game_id) =>
+  `${base_url}games/${game_id}?key=${process.env.REACT_APP_RAWG_API}`;
+//Game screenshots
+export const gameScreenshotURL = (game_id) =>
+  `${base_url}games/${game_id}/screenshots?key=${process.env.REACT_APP_RAWG_API}`;
