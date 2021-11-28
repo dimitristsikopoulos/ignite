7216ca9aca8136c91852a794158a9500a083d8ac
Dimitrios Tsikopoulos <dimitristsik@gmail.com>
2021-11-28 09:45:58 +0200

    4. Action Creators,Redux and Thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 //REDUX SETUP
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const composeEnchanser = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnchanser(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
